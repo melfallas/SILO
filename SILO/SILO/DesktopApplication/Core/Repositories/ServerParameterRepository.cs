2f75aba42e4a6c5f5d8c6e6e83862048b514632a
Melvin Fallas <melfallas@yahoo.com>
2019-05-10 19:17:16 -0600

    E. 2019. SILO. Parameter Classes. Nuevas clases parámetros de servidor.
    E. 2019. SILO. Parameter Classes. Nuevas clases para parámetros de servidor.

---
+++ SILO/SILO/DesktopApplication/Core/Repositories/ServerParameterRepository.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SILO.DesktopApplication.Core.Repositories
+{
+    public class ServerParameterRepository
+    {
+        public List<SPR_ServerParameter> getAll()
+        {
+            List<SPR_ServerParameter> paramList = null;
+            using (var context = new SILOEntities())
+            {
+                paramList = context.SPR_ServerParameter.ToList();
+            }
+            return paramList;
+        }
+
+        public string getParamValue(string pName)
+        {
+            SPR_ServerParameter findedParam = this.getByName(pName);
+            return findedParam == null ? "" : findedParam.SPR_Value.Trim();
+        }
+
+        public SPR_ServerParameter getByName(string pName)
+        {
+            SPR_ServerParameter paramValue = null;
+            try
+            {
+                using (var context = new SILOEntities())
+                {
+                    List<SPR_ServerParameter> paramList = context.SPR_ServerParameter
+                        .Where(param => param.SPR_Name == pName).ToList();
+                    if (paramList.Count > 0)
+                    {
+                        paramValue = paramList[0];
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            return paramValue;
+        }
+
+        public void save(SPR_ServerParameter pParam)
+        {
+            SPR_ServerParameter parameter = null;
+            using (var context = new SILOEntities())
+            {
+                List<SPR_ServerParameter> paramList = context.SPR_ServerParameter
+                        .Where(param => param.SPR_Name == pParam.SPR_Name).ToList();
+                if (paramList.Count > 0)
+                {
+                    parameter = paramList[0];
+                }
+                if (parameter == null)
+                {
+                    parameter = new SPR_ServerParameter();
+                    parameter.SPR_Name = pParam.SPR_Name;
+                    parameter.SPR_Value = pParam.SPR_Value;
+                }
+                else
+                {
+                    parameter.SPR_Value = pParam.SPR_Value;
+                }
+                context.SaveChanges();
+            }
+        }
+
+    }
+}
---
+++ SILO/SILO/DesktopApplication/Core/Services/ServerParameterService.cs
@@ -0,0 +1,35 @@
+﻿using SILO.DesktopApplication.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SILO.DesktopApplication.Core.Services
+{
+    public class ServerParameterService
+    {
+
+        public static SPR_ServerParameter getLocalParameter(string pParamName)
+        {
+            ServerParameterRepository serverParamRepo = new ServerParameterRepository();
+            return serverParamRepo.getByName(pParamName);
+        }
+
+        public static string getLocalParameterValue(string pParamName)
+        {
+            ServerParameterRepository serverParamRepo = new ServerParameterRepository();
+            return serverParamRepo.getParamValue(pParamName);
+        }
+
+        public static void setLocalParameterValue(string pParamName, string pParamValue)
+        {
+            ServerParameterRepository serverParamRepo = new ServerParameterRepository();
+            SPR_ServerParameter serverParam = new SPR_ServerParameter();
+            serverParam.SPR_Name = pParamName;
+            serverParam.SPR_Value = pParamValue;
+            serverParamRepo.save(serverParam);
+        }
+
+    }
+}
